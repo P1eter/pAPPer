b890744b96edc22c4da981f82c8a4e111d092556
Pieter <p.kronemeijer@xs4all.nl>
2018-01-26 20:32:31 +0100

    added discovered robots to the spinner at robotSelection

--- pAPPer/app/src/main/java/com/example/pieter/papper/RobotSelectFragment.java
+++ pAPPer/app/src/main/java/com/example/pieter/papper/RobotSelectFragment.java
@@ -9,7 +9,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 
 /**
@@ -21,17 +26,29 @@ public class RobotSelectFragment extends DialogFragment implements View.OnClickL
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_robot_select, container, false);
     }
-
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        Bundle bundle = getArguments();
         view.findViewById(R.id.connect_button).setOnClickListener(this);
         view.findViewById(R.id.disconnect_button).setOnClickListener(this);
+
+        Spinner robotSpinner = view.findViewById(R.id.robot_select_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_item, bundle.getStringArrayList("robots"));
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        robotSpinner.setAdapter(adapter);
     }
+
 
     @Override
     public void onClick(View view) {
