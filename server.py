c93977cfe33a213e4646446d47f414adb809cead
Pieter <p.kronemeijer@xs4all.nl>
2018-02-01 14:14:24 +0100

    cleaned server

--- server.py
+++ server.py
@@ -4,9 +4,10 @@ from naoqi import ALProxy
 
 class Server():
     def __init__(self):
-        self.HOST = ''   # Symbolic name meaning all available interfaces
-        self.PORT = 1717
+        self.HOST = ''   # symbolic name meaning all available interfaces
+        self.PORT = 1717 # always 1717
 
+        # make proxies to naoqi
         self.tts = ALProxy("ALTextToSpeech", "localhost", 9559)
         self.move = ALProxy("ALMotion", "localhost", 9559)
         self.autl = ALProxy("ALAutonomousLife", "localhost", 9559)
@@ -15,27 +16,60 @@ class Server():
 
         sys.path.append('dances')
 
+        # create socket
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 9999)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         print 'Socket created'
  
-        #Bind socket to local host and port
+    def startServer():
+        # bind socket to host and port
         try:
             self.s.bind((self.HOST, self.PORT))
         except socket.error as msg:
             print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
             sys.exit()
-
         print 'Socket bind complete'
 
-        #Start listening on socket
+        # start listening on socket
         self.s.listen(0)
         print 'Socket now listening'
 
+    def talk(self, data):
+        volume, animated_speech = data.split(" ")[:2]
+        volume, animated_speech = int(volume), bool(int(animated_speech))
+        # data_string[6+len(str(volume)):]
+        text = data[3+len(str(volume)):]
+        
+        print "Setting volume to", volume
+        self.tts.setVolume(round(volume / 10.0, 1))
+
+        print "animated speech:", animated_speech
+
+        if animated_speech:
+            self.ansp.say(text)
+        else:
+            self.tts.say(text)
+
+    def move(self, data):
+        x, y, theta = data.split(" ")
+        x, y, theta = float(x), float(y), float(theta)
+
+        if not self.move.robotIsWakeUp():
+            print "waking Pepper up"
+            self.move.wakeUp()
+
+        # check for valid parameters
+        if x <= 1 and x >= -1 and y <= 1 and y >= -1 and theta <= 1 and theta > -1:
+            self.move.moveToward(float(x), float(y), float(theta))
+            print "moving:", data
+
+    def wakeUp(self, data):
+        self.move.wakeUp() if data == "1" else self.move.rest()
+
     def dance(self, dance):
-        print dance
         try :
+            # get interpolation data from dance files
             variables = {}
             execfile("dances/" + dance + ".py", variables)
 
@@ -43,59 +77,40 @@ class Server():
                 print "waking pepper up"
                 self.move.wakeUp()
 
+            # TODO: fix
             if dance == "saxophone":
                 self.ap.playFile("/home/nao/sounds/epicsax.ogg")
 
+            # execute interpolation
             self.move.angleInterpolationBezier(variables["names"], variables["times"], variables["keys"])
         except Exception, e:
             print e
 
+    def setAutonomousLife(self, data):
+            if data == "1":
+                print "entering autonomous life state"
+                self.autl.setState("interactive")
+            elif data == "0":
+                print "exiting autonomous life state"
+                self.autl.setState("disabled")
+
     def handleData(self, data_string):
         if data_string[:4] == "talk":
-            volume, animated_speech = data_string[5:].split(" ")[:2]
-            volume, animated_speech = int(volume), bool(int(animated_speech))
-            # data_string[6+len(str(volume)):]
-            text = data_string[8+len(str(volume)):]
-            
-            print "Setting volume to", volume
-            self.tts.setVolume(round(volume / 10.0, 1))
-
-            print "animated speech:", animated_speech
-
-            if animated_speech:
-                self.ansp.say(text)
-            else:
-                self.tts.say(text)
+            self.talk(data_string[5:])
         elif data_string[:4] == "move":
-            x, y, theta = data_string[5:].split(" ")
-            x, y, theta = float(x), float(y), float(theta)
-            # print(split_move_command)
-            if not self.move.robotIsWakeUp():
-                print "waking Pepper up"
-                self.move.wakeUp()
-            if x <= 1 and x >= -1 and y <= 1 and y >= -1 and theta <= 1 and theta > -1:
-                self.move.moveToward(float(x), float(y), float(theta))
-                print "moving:", data_string[5:]
+            self.move(data_string[5:])
         elif data_string[:4] == "wake":
-            self.move.wakeUp() if data_string[5] == "1" else self.move.rest()
+            self.wakeUp(data_string[5])
         elif data_string[:4] == "danc":
             self.dance(data_string[5:])
         elif data_string[:4] == "autl":
-            if data_string[5] == "1":
-                print "entering autonomous life state"
-                self.autl.setState("interactive")
-            elif data_string[5] == "0":
-                print "exiting autonomous life state"
-                self.autl.setState("disabled")
+            self.setAutonomousLife(data_string[5])
 
     def preprocessData(self, data_string):
         # only execute the first command that was sent
         return data_string.split('\n')[0]
 
-    def sendAlive(self):
-        self.conn.sendall("alive")
-
-    #Function for handling connections. This will be used to create threads
+    # function for handling connections
     def handleClient(self, conn):
         #Sending message to connected client
         # self.conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
@@ -120,14 +135,16 @@ class Server():
             data_string = self.preprocessData(data_string)
             self.handleData(data_string)
          
-        #came out of loop
+        # came out of loop
         self.conn.close()
         self.tts.say("Disconnected")
 
     def run(self):
-        #now keep talking with the client
+        self.startServer()
+
+        # now keep talking with the client
         while 1:
-            #wait to accept a connection - blocking call
+            # wait to accept a connection - blocking call
             self.conn, self.addr = self.s.accept()
             print 'Connected with ' + self.addr[0] + ':' + str(self.addr[1])
 
@@ -136,7 +153,6 @@ class Server():
             print "listening for connections again"     
 
         self.s.close()
-
 
 
 if __name__ == "__main__":
